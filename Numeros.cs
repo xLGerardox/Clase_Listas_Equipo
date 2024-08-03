9badb2e3684b5e8f0dd2ed5e34a276388b4f0c7b
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-02 19:46:21 -0600

    Se creo un nuevo proyecto porque no me dejaba hacer push en el primer repositorio

--- Numeros.cs
+++ Numeros.cs
@@ -8,5 +8,105 @@ namespace ConsoleApp17
 {
     internal class Numeros
     {
+        public int Numero { get; set; }
+
+        public Numeros(int numero)
+        {
+            Numero = numero;
+        }
+
+        public static void IngresarNumeros(List<Numeros> listaNumeros)
+        {
+            try
+            {
+                Console.WriteLine("Ingrese un número:");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                Numeros numero1 = new Numeros(numero);
+                listaNumeros.Add(numero1);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Por favor, ingrese un número entero.");
+            }
+        }
+
+        public static void MostrarNumeros(List<Numeros> listaNumeros)
+        {
+            Console.WriteLine("Lista de números:");
+            foreach (Numeros numero in listaNumeros)
+            {
+                Console.WriteLine(numero.Numero);
+            }
+        }
+
+        public static void SumarNumeros(List<Numeros> listaNumeros)
+        {
+            int suma = 0;
+            foreach (Numeros numero in listaNumeros)
+            {
+                suma += numero.Numero;
+            }
+            Console.WriteLine($"La suma de los números es: {suma}");
+        }
+
+        public static void RestarNumeros(List<Numeros> listaNumeros)
+        {
+            if (listaNumeros.Count > 0)
+            {
+                int resta = listaNumeros[0].Numero;
+                for (int i = 1; i < listaNumeros.Count; i++)
+                {
+                    resta -= listaNumeros[i].Numero;
+                }
+                Console.WriteLine($"La resta de los números es: {resta}");
+            }
+            else
+            {
+                Console.WriteLine("No hay números en la lista para restar.");
+            }
+        }
+
+        public static void MultiplicarNumeros(List<Numeros> listaNumeros)
+        {
+            int multiplicacion = 1;
+            foreach (Numeros numero in listaNumeros)
+            {
+                multiplicacion *= numero.Numero;
+            }
+            Console.WriteLine($"La multiplicación de los números es: {multiplicacion}");
+        }
+
+        public static void DividirNumeros(List<Numeros> listaNumeros)
+        {
+            if (listaNumeros.Count > 0)
+            {
+                float division = listaNumeros[0].Numero;
+                for (int i = 1; i < listaNumeros.Count; i++)
+                {
+                    if (listaNumeros[i].Numero != 0)
+                    {
+                        division /= listaNumeros[i].Numero;
+                    }
+                    else
+                    {
+                        Console.WriteLine("resultado indefinido.");
+                        return;
+                    }
+                }
+                Console.WriteLine($"La división de los números es: {division}");
+            }
+            else
+            {
+                Console.WriteLine("No hay números en la lista para dividir.");
+            }
+        }
+
+        public static void MostrarOperaciones(List<Numeros> listaNumeros)
+        {
+            SumarNumeros(listaNumeros);
+            RestarNumeros(listaNumeros);
+            MultiplicarNumeros(listaNumeros);
+            DividirNumeros(listaNumeros);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp17;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Numeros> listaNumeros = new List<Numeros>();
+        bool menu = true;
+        int opcion;
+
+        while (menu)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Ingresar números");
+            Console.WriteLine("2. Mostrar números");
+            Console.WriteLine("3. Sumar números");
+            Console.WriteLine("4. Restar números");
+            Console.WriteLine("5. Multiplicar números");
+            Console.WriteLine("6. Dividir números");
+            Console.WriteLine("7. Mostrar todas las operaciones");
+            Console.WriteLine("8. Salir");
+            Console.Write("Seleccione una opción: ");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.Clear();
+                    Numeros.IngresarNumeros(listaNumeros);
+                    break;
+                case 2:
+                    Console.Clear();
+                    Numeros.MostrarNumeros(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    Console.Clear();
+                    Numeros.SumarNumeros(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    Console.Clear();
+                    Numeros.RestarNumeros(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 5:
+                    Console.Clear();
+                    Numeros.MultiplicarNumeros(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 6:
+                    Console.Clear();
+                    Numeros.DividirNumeros(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 7:
+                    Console.Clear();
+                    Numeros.MostrarOperaciones(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 8:
+                    menu = false;
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Presione cualquier tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
